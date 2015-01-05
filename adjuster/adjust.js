238bd8bbab005af65537e3a1462c3730c4c697cf
zachwe <zbwener@gmail.com>
2015-01-05 14:08:35 -0500

    added methods to get date range and frequency

--- adjuster/adjust.js
+++ adjuster/adjust.js
@@ -1,15 +1,51 @@
 var request = require('request');
 var ndarray = require('ndarray');
 
-var Adjuster = function(data) {
+var Adjuster = function(data, frequency) {
+    // Let's have data be an array of arrays. [[d1, v1], [d2, v2], ...]
     this.data = data;
-    this.FRED_API_KEY = "ced316f0b3196133c55f5febd1003f88",
-        this.FRED_REST_ENDPT = "http://api.stlouisfed.org/fred/series/observations";
+    this.FRED_API_KEY = process.env.FRED_API_KEY,
+        this.FRED_REST_ENDPT = "http://api.stlouisfed.org/fred/series/observations",
         this.CPI_SERIES_ID = "CPIAUCSL";
 
+    this.dateRange = (function(d) {
+        var max = 0,
+            min = Number.MAX_SAFE_INTEGER;
+        d.forEach(function(v, i, ar) {
+            var time = v[0].getTime();
+            if(time > max) {
+                max = v[0];
+            }
+            if(time < min) {
+                min = v[0];
+            }
+        });
+        return [min, max];
+    })(this.data);
+
+    this.frequency = frequency || (function(d) {
+        var dateDict = {};
+        d.forEach(function(v, i, ar) {
+            var thisYear = v[0].getFullYear();
+            var thisMonth = v[0].getMonth();
+            if(thisYear in dateDict && dateDict[thisYear] != thisMonth) {
+                dateDict[v[0].getFullYear()] = thisMonth;
+            } else {
+                return "m";
+            }
+        });
+        return "a";
+    })(this.data);
 
     this.getInflationNumbers = function(startdate, enddate, freq, agg) {
+        // The FRED api will give you dates on the first of the month. So, we
+        // don't have to do any hard work in enforcing compliance... if someone
+        // gives us a date on August 18, just send it in as-is and the api will give
+        // back August 1. 
+        // m, q, sa, or a.
+        // For monthly, quarterly, semi-annual, or annual.
         var frequency = freq || "a", 
+        // sum, eop, avg. Only use avg realistically.
             aggmethod = agg || "avg";
         var units = "lin";
         var params = {observation_start: startdate, observation_end: enddate,
--- adjuster/routes/adjust.js
+++ adjuster/routes/adjust.js
@@ -3,7 +3,7 @@ var adjuster = require('../adjust');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    adjuster.getInflationNumbers("2010-01-01", "2014-01-01");
+    adjuster.getInflationNumbers("2010-01-01", "2014-11-17", "m");
     res.send("adjust");
 });
 
