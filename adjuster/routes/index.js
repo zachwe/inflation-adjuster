32d0984c3a18fc5f56de1a946982ec87fc109f68
zachwe <zbwener@gmail.com>
2015-01-20 13:02:25 -0500

    added example file and adjusted UI

--- adjuster/routes/index.js
+++ adjuster/routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 
 /* GET home page. */
 router.get('/', function(req, res) {
     res.render('index', {display_output: "none"});
+});
+
+router.get('/cpi', function(req, res){
+    fs.readFile("./public/files/cpi.csv", function(err, data) {
+        res.write(data);
+        res.end();
+    });
 });
 
 module.exports = router;
