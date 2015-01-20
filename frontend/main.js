1db6af023e8fde8c801c3e691b3019a110edbe94
zachwe <zbwener@gmail.com>
2015-01-20 17:20:20 -0500

    add key interaction and copy changes.

--- frontend/main.js
+++ frontend/main.js
@@ -2,22 +2,18 @@ var $ = require("jquery-browserify");
 
 $(document).ready(function() {
     console.log("ready");
+    $(".data-form").on("change", "#data_area", function() {
+        $(".data-form").submit();
+    });
+    $(".data-form").on("keypress", "#data_area", function(eventData) {
+        if(eventData.which == 13) {
+            $(".data-form").submit();
+        }
+    });
+    $(".data-form").on("change", "#fileinput", function() {
+        $(".data-form").submit();
+    });
     $(".data-form").submit(function(event) {
-        //event.preventDefault();
-        /*
-        var formData = new FormData(this);
-        $.ajax({
-            url: "/adjust/data",
-            type: "POST",
-            success: function(data) {
-                console.log("success!");
-                console.log(data);
-            },
-            data: formData,
-            contentTpe: false,
-            processData: false
-        });
-        */
         console.log("submitted form");
     });
 
